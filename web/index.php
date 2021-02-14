9e1aa0fb512c15ea039a8761c3ee965d68cf3b58
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-02-13 23:31:00 -0700

    forgot to add the viewable link

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
       <h1> This is take 2</h1>
-        <a href="hello.html">
+        <a href="hello.html">Hello page</a>
           <br>
         <?php
         echo "Today is " . date("Y/m/d") . "<br>";
