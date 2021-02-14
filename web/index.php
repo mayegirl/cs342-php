55a83883ad216d02ea8ec2160cd82995920699f9
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-02-13 23:27:06 -0700

    testing new homepage

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Michaela Smith's Homepage</title>
+    </head>
+    <body>
+      <h1> This is take 2</h1>
+        <a href="hello.html">
+          <br>
+        <?php
+        echo "Today is " . date("Y/m/d") . "<br>";
+        ?>
+    </body>
+</html>
